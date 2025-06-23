22a8e3d1590245bcbd631e97fac14edaaeb32b3a
habi802 <shb8002@gmail.com>
2025-06-23 13:19:40 +0900

    [feat] 반응형 상태, 컴퓨티드, 데이터 바인딩, 훅, 스토어

--- src/views/Component.vue
+++ src/views/Component.vue
@@ -1,12 +1,15 @@
 <script setup>
 import FrontWheel from "@/components/FrontWheel.vue";
 import RearWheel from "@/components/RearWheel.vue";
+import { useCounterStore } from "@/stores/counter";
+const counterStore = useCounterStore();
 </script>
 
 <template>
   <h1>Hello Vue!</h1>
   <!-- <FrontWheel></FrontWheel>
   <RearWheel></RearWheel> -->
+  <button @click="counterStore.increment">회전</button>
   <div class="car">
     <FrontWheel />
     <FrontWheel />
